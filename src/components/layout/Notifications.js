b3aec4773ca37c3c30d5fd7177d20acc70807b53
robertmoore40 <robertcmoore92@gmail.com>
2020-08-30 11:33:38 -0400

    material imports

--- src/components/layout/Notifications.js
+++ src/components/layout/Notifications.js
@@ -4,3 +4,6 @@ import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
+
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
