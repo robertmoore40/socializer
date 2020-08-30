6ae07c5f66a84aa7590cd88e7e4ea4fe0c5a6e7d
robertmoore40 <robertcmoore92@gmail.com>
2020-08-30 11:34:10 -0400

    added material imports

--- src/components/layout/Notifications.js
+++ src/components/layout/Notifications.js
@@ -9,3 +9,5 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
