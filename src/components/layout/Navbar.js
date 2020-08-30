6043df28dbd39196de3aa19774ac0ac24fabe014
robertmoore40 <robertcmoore92@gmail.com>
2020-08-30 11:32:07 -0400

    navbar components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -14,7 +14,38 @@ import HomeIcon from '@material-ui/icons/Home';
 
 
 class Navbar extends Component {
-
+  render() {
+    const { authenticated } = this.props;
+    return (
+      <AppBar>
+        <Toolbar className="nav-container">
+          {authenticated ? (
+            <Fragment>
+              <PostScream />
+              <Link to="/">
+                <MyButton tip="Home">
+                  <HomeIcon />
+                </MyButton>
+              </Link>
+              <Notifications />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+            </Fragment>
+          )}
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
 Navbar.propTypes = {
