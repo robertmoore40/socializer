9dc87fb788913591f846c4a6f4d0db1a2976812b
robertmoore40 <robertcmoore92@gmail.com>
2020-08-31 14:37:29 -0400

    import

--- src/App.js
+++ src/App.js
@@ -3,3 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import jwtDecode from 'jwt-decode';
