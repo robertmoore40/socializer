06501ed8f7e45feb419473cd7bf11444759bdf73
robertmoore40 <robertcmoore92@gmail.com>
2020-08-31 14:37:51 -0400

    added redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import './App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import jwtDecode from 'jwt-decode';
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
