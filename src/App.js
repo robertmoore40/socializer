786fc94f968384598be2e865a0c53440fc537e6d
robertmoore40 <robertcmoore92@gmail.com>
2020-08-31 14:37:16 -0400

    import

--- src/App.js
+++ src/App.js
@@ -1,2 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
