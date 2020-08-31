c610c7e58c82af0c4a96f639f0b49aab27669478
robertmoore40 <robertcmoore92@gmail.com>
2020-08-31 14:38:11 -0400

    redux

--- src/App.js
+++ src/App.js
@@ -7,3 +7,5 @@ import jwtDecode from 'jwt-decode';
 
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
