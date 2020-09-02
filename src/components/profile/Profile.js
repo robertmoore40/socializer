3220229b1a37b5d64616907cbf92ee88f2ea9285
robertmoore40 <robertcmoore92@gmail.com>
2020-09-02 07:45:34 -0400

    added materialize

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,3 +6,8 @@ import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import EditDetails from './EditDetails';
 import MyButton from '../../util/MyButton';
+import ProfileSkeleton from '../../util/ProfileSkeleton';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import MuiLink from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
