b78abbf4f2502c2d5704c7436958fac551b006d1
robertmoore40 <robertcmoore92@gmail.com>
2020-09-02 07:46:01 -0400

    added icons

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -11,3 +11,6 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+import LocationOn from '@material-ui/icons/LocationOn';
+import LinkIcon from '@material-ui/icons/Link';
+import CalendarToday from '@material-ui/icons/CalendarToday';
