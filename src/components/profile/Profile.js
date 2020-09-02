96e712b2ba8426718601688dadc7815ac45aed10
robertmoore40 <robertcmoore92@gmail.com>
2020-09-02 07:45:03 -0400

    added day and imported more functions

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,3 +3,6 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import EditDetails from './EditDetails';
+import MyButton from '../../util/MyButton';
