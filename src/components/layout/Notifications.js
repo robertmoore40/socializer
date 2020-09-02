0065019a5a9d475d2b8c7b5b3fca270b464f7e0e
robertmoore40 <robertcmoore92@gmail.com>
2020-09-02 07:40:30 -0400

    added render

--- src/components/layout/Notifications.js
+++ src/components/layout/Notifications.js
@@ -39,69 +39,78 @@ class Notifications extends Component {
       render() {
         const notifications = this.props.notifications;
         const anchorEl = this.state.anchorEl;
-
+    
         dayjs.extend(relativeTime);
-
+    
         let notificationsIcon;
         if (notifications && notifications.length > 0) {
           notifications.filter((not) => not.read === false).length > 0
             ? (notificationsIcon = (
                 <Badge
-                badgeContent={
-                  notifications.filter((not) => not.read === false).length
-                }
-                color="secondary"
-              >
-                <NotificationsIcon />
-              </Badge>
-                ))
-                : (notificationsIcon = <NotificationsIcon />);
-            } else {
-              notificationsIcon = <NotificationsIcon />;
-            }
-            let notificationsMarkup =
-            notifications && notifications.length > 0 ? (
-                notifications.map((not) => {
-                    const verb = not.type === 'like' ? 'liked' : 'commented on';
-                    const time = dayjs(not.createdAt).fromNow();
-                    const iconColor = not.read ? 'primary' : 'secondary';
-                    const icon =
-                      not.type === 'like' ? (
-                        <FavoriteIcon color={iconColor} style={{ marginRight: 10 }} />
-                        ) : (
-                          <ChatIcon color={iconColor} style={{ marginRight: 10 }} />
-                        );
-
-                        return (
-                            <MenuItem key={not.createdAt} onClick={this.handleClose}>
-                              {icon}
-                              <Typography
-                component={Link}
-                color="default"
-                variant="body1"
-                to={`/users/${not.recipient}/scream/${not.screamId}`}
-              >
-                {not.sender} {verb} your scream {time}
-              </Typography>
+                  badgeContent={
+                    notifications.filter((not) => not.read === false).length
+                  }
+                  color="secondary"
+                >
+                  <NotificationsIcon />
+                </Badge>
+              ))
+            : (notificationsIcon = <NotificationsIcon />);
+        } else {
+          notificationsIcon = <NotificationsIcon />;
+        }
+        let notificationsMarkup =
+          notifications && notifications.length > 0 ? (
+            notifications.map((not) => {
+              const verb = not.type === 'like' ? 'liked' : 'commented on';
+              const time = dayjs(not.createdAt).fromNow();
+              const iconColor = not.read ? 'primary' : 'secondary';
+              const icon =
+                not.type === 'like' ? (
+                  <FavoriteIcon color={iconColor} style={{ marginRight: 10 }} />
+                ) : (
+                  <ChatIcon color={iconColor} style={{ marginRight: 10 }} />
+                );
+    
+              return (
+                <MenuItem key={not.createdAt} onClick={this.handleClose}>
+                  {icon}
+                  <Typography
+                    component={Link}
+                    color="default"
+                    variant="body1"
+                    to={`/users/${not.recipient}/scream/${not.screamId}`}
+                  >
+                    {not.sender} {verb} your scream {time}
+                  </Typography>
+                </MenuItem>
+              );
+            })
+          ) : (
+            <MenuItem onClick={this.handleClose}>
+              You have no notifications yet
             </MenuItem>
           );
-        })
-        ) : (
-          <MenuItem onClick={this.handleClose}>
-            You have no notifications yet
-          </MenuItem>
-            );
-            return (
-              <Fragment>
-                <Tooltip placement="top" title="Notifications">
-                  <IconButton
-                    aria-owns={anchorEl ? 'simple-menu' : undefined}
-                    aria-haspopup="true"
-                    onClick={this.handleOpen}
-                  >
-                    {notificationsIcon}
-                    );
-                    </Fragment>
-    );
-  }
-}
+        return (
+          <Fragment>
+            <Tooltip placement="top" title="Notifications">
+              <IconButton
+                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup="true"
+                onClick={this.handleOpen}
+              >
+                {notificationsIcon}
+              </IconButton>
+            </Tooltip>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={this.handleClose}
+              onEntered={this.onMenuOpened}
+            >
+              {notificationsMarkup}
+            </Menu>
+          </Fragment>
+        );
+      }
+    }
