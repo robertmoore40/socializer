4cdd3328c3841cff7d06a899ca73c993237058ba
robertmoore40 <robertcmoore92@gmail.com>
2020-09-02 07:44:33 -0400

    link and style

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,3 +1,5 @@
   
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
