aabcb1b2b435e58993f48d72aa7edd5d3158b7c8
robertmoore40 <robertcmoore92@gmail.com>
2020-09-05 12:13:15 -0400

    additional import

--- src/components/profile/StaticProfile.js
+++ src/components/profile/StaticProfile.js
@@ -1,2 +1,5 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import dayjs from 'dayjs';
+import { Link } from 'react-router-dom';
