022802b4533e68647f59d035b7f3ba68eedc6700
robertmoore40 <robertcmoore92@gmail.com>
2020-09-05 12:19:19 -0400

    added material

--- src/components/profile/EditDetails.js
+++ src/components/profile/EditDetails.js
@@ -11,3 +11,7 @@ import TextField from '@material-ui/core/TextField';
 
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import EditIcon from '@material-ui/icons/Edit';
