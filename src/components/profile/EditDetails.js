0f705b1cda508b2e723b4a73dd725a155b7344e0
robertmoore40 <robertcmoore92@gmail.com>
2020-09-05 12:18:15 -0400

    added dialog

--- src/components/profile/EditDetails.js
+++ src/components/profile/EditDetails.js
@@ -5,3 +5,9 @@ import MyButton from '../../util/MyButton';
 
 import { connect } from 'react-redux';
 import { editUserDetails } from '../../redux/actions/userActions';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
