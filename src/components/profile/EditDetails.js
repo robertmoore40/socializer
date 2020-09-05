5534a168767bd3e5bf741e2c07df95b9bda4a8dc
robertmoore40 <robertcmoore92@gmail.com>
2020-09-05 12:17:21 -0400

    added redux

--- src/components/profile/EditDetails.js
+++ src/components/profile/EditDetails.js
@@ -1,2 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import MyButton from '../../util/MyButton';
+
+import { connect } from 'react-redux';
+import { editUserDetails } from '../../redux/actions/userActions';
