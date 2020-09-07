20c45773f29b45d82042043872e23817293f8542
robertmoore40 <robertcmoore92@gmail.com>
2020-09-07 17:28:32 -0400

    added const styles with theme

--- src/components/profile/EditDetails.js
+++ src/components/profile/EditDetails.js
@@ -15,3 +15,10 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 import EditIcon from '@material-ui/icons/Edit';
+
+const styles = (theme) => ({
+    ...theme,
+    button: {
+      float: 'right'
+    }
+  });
