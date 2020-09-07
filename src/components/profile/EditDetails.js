ba321927fe03ddbbc78ab4271f42f1d4de09be35
robertmoore40 <robertcmoore92@gmail.com>
2020-09-07 17:30:46 -0400

    formatted document

--- src/components/profile/EditDetails.js
+++ src/components/profile/EditDetails.js
@@ -1,3 +1,4 @@
+  
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -8,7 +9,6 @@ import { editUserDetails } from '../../redux/actions/userActions';
 
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -17,28 +17,27 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import EditIcon from '@material-ui/icons/Edit';
 
 const styles = (theme) => ({
-    ...theme,
-    button: {
-      float: 'right'
-    }
-  });
+  ...theme,
+  button: {
+    float: 'right'
+  }
+});
 
-  class EditDetails extends Component {
-    state = {
-      bio: '',
-      website: '',
-      location: '',
-      open: false
-    };
-
-    mapUserDetailsToState = (credentials) => {
-        this.setState({
-            bio: credentials.bio ? credentials.bio : '',
-            website: credentials.website ? credentials.website : '',
-            location: credentials.location ? credentials.location : ''
-          });
-        };
-        handleOpen = () => {
-          this.setState({ open: true });
-          this.mapUserDetailsToState(this.props.credentials);
-        };
+class EditDetails extends Component {
+  state = {
+    bio: '',
+    website: '',
+    location: '',
+    open: false
+  };
+  mapUserDetailsToState = (credentials) => {
+    this.setState({
+      bio: credentials.bio ? credentials.bio : '',
+      website: credentials.website ? credentials.website : '',
+      location: credentials.location ? credentials.location : ''
+    });
+  };
+  handleOpen = () => {
+    this.setState({ open: true });
+    this.mapUserDetailsToState(this.props.credentials);
+  };
