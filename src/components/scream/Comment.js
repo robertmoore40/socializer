fed80898f0eb5dec7ee0bfc4fce9069634daff42
robertmoore40 <robertcmoore92@gmail.com>
2020-09-09 17:34:22 -0400

    added time

--- src/components/scream/Comment.js
+++ src/components/scream/Comment.js
@@ -2,3 +2,4 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
