9c72aba1c4684f1388f8696c8e4b0e9a422d8ad2
robertmoore40 <robertcmoore92@gmail.com>
2020-09-10 17:06:01 -0400

    added styles

--- src/components/scream/Comment.js
+++ src/components/scream/Comment.js
@@ -5,3 +5,16 @@ import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+
+const styles = (theme) => ({
+  ...theme,
+  commentImage: {
+    maxWidth: '100%',
+    height: 100,
+    objectFit: 'cover',
+    borderRadius: '50%'
+  },
+  commentData: {
+    marginLeft: 20
+  }
+});
