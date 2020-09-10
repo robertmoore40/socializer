3250fc12d5ebb15e2c9838490103c2597f11e9e0
robertmoore40 <robertcmoore92@gmail.com>
2020-09-10 17:13:21 -0400

    imported redux

--- src/components/scream/CommentForm.js
+++ src/components/scream/CommentForm.js
@@ -5,3 +5,6 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+
+import { connect } from 'react-redux';
+import { submitComment } from '../../redux/actions/dataActions';
