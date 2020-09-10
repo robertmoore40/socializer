3bae807f1d1ec81da28cc4624d3c43d38adbc99c
robertmoore40 <robertcmoore92@gmail.com>
2020-09-10 17:13:03 -0400

    aded materialize imports

--- src/components/scream/CommentForm.js
+++ src/components/scream/CommentForm.js
@@ -1,3 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
