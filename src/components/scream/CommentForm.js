8f5c2a3722ac2e40944dec49668ecebb0aae1ad0
robertmoore40 <robertcmoore92@gmail.com>
2020-09-11 09:04:53 -0400

    added text field info

--- src/components/scream/CommentForm.js
+++ src/components/scream/CommentForm.js
@@ -43,3 +43,14 @@ class CommentForm extends Component {
         
         <Grid item sm={12} style={{ textAlign: 'center' }}>
           <form onSubmit={this.handleSubmit}>
+          <TextField
+            name="body"
+            type="text"
+            label="Comment on scream"
+            error={errors.comment ? true : false}
+            helperText={errors.comment}
+            value={this.state.body}
+            onChange={this.handleChange}
+            fullWidth
+            className={classes.textField}
+          />
