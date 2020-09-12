019a3437ba9ed402255ea8fb34b1fdc4df758118
robertmoore40 <robertcmoore92@gmail.com>
2020-09-12 15:23:41 -0400

    removed extra format space

--- src/components/scream/LikeButton.js
+++ src/components/scream/LikeButton.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import MyButton from '../../util/MyButton';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-
 import { connect } from 'react-redux';
 import { likeScream, unlikeScream } from '../../redux/actions/dataActions';
 
