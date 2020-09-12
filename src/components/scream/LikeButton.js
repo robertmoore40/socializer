14a548477700184d6d61bf8d8272bc8a6bd6e31e
robertmoore40 <robertcmoore92@gmail.com>
2020-09-12 15:22:07 -0400

    added materialize icons

--- src/components/scream/LikeButton.js
+++ src/components/scream/LikeButton.js
@@ -2,3 +2,6 @@ import React, { Component } from 'react';
 import MyButton from '../../util/MyButton';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
