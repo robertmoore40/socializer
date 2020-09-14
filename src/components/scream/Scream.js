410c4debe4714b178e3b97b825a7a8ab179415e0
robertmoore40 <robertcmoore92@gmail.com>
2020-09-14 09:40:34 -0400

    add buttons

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -4,3 +4,4 @@ import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
+import MyButton from '../../util/MyButton';
