f2326627d65161a79b01787af18af704a7dc7bb4
robertmoore40 <robertcmoore92@gmail.com>
2020-09-15 10:57:04 -0400

    added icon

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -12,3 +12,4 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import ChatIcon from '@material-ui/icons/Chat';
