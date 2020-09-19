8bc555c39f6c89dd56010eb9cb3921b31649cab1
robertmoore40 <robertcmoore92@gmail.com>
2020-09-19 15:09:09 -0400

    formatted

--- src/pages/user.js
+++ src/pages/user.js
@@ -17,18 +17,19 @@ class home extends Component {
       const { screams, loading } = this.props.data;
       let recentScreamsMarkup = !loading ? (
         screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
-        ) : (
-          <ScreamSkeleton />
-          );
-    return (
+      ) : (
+        <ScreamSkeleton />
+      );
+      return (
         <Grid container spacing={16}>
-        <Grid item sm={8} xs={12}>
-          {recentScreamsMarkup}
+          <Grid item sm={8} xs={12}>
+            {recentScreamsMarkup}
+          </Grid>
+          <Grid item sm={4} xs={12}>
+            <Profile />
+          </Grid>
         </Grid>
-        <Grid item sm={4} xs={12}>
-        <Profile />
-        </Grid>
-      </Grid>
-    );
+      );
+    }
   }
-}
+  
