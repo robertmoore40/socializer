723b78827a79d7101c55f77a5eb4024c2567bb0f
robertmoore40 <robertcmoore92@gmail.com>
2020-09-22 08:44:55 -0400

    added util

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,3 +3,4 @@ import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import Scream from '../components/scream/Scream';
 import Profile from '../components/profile/Profile';
+import ScreamSkeleton from '../util/ScreamSkeleton';
