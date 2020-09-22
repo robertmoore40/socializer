bddfe271b17103158fd022be2909214971aa6f1e
robertmoore40 <robertcmoore92@gmail.com>
2020-09-22 08:41:53 -0400

    reformatted

--- src/pages/user.js
+++ src/pages/user.js
@@ -9,38 +9,72 @@ import ScreamSkeleton from '../util/ScreamSkeleton';
 import { connect } from 'react-redux';
 import { getScreams } from '../redux/actions/dataActions';
 
-class home extends Component {
-    componentDidMount() {
-      this.props.getScreams();
-    }
-    render() {
-      const { screams, loading } = this.props.data;
-      let recentScreamsMarkup = !loading ? (
-        screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
-      ) : (
-        <ScreamSkeleton />
-      );
-      return (
-        <Grid container spacing={16}>
-          <Grid item sm={8} xs={12}>
-            {recentScreamsMarkup}
-          </Grid>
-          <Grid item sm={4} xs={12}>
-            <Profile />
-          </Grid>
-        </Grid>
-      );
-    }
-  }
-  
-  user.propTypes = {
-    getUserData: PropTypes.func.isRequired,
-    data: PropTypes.object.isRequired
+class user extends Component {
+  state = {
+    profile: null,
+    screamIdParam: null
   };
-  const mapStateToProps = (state) => ({
-    data: state.data
-  });
-  export default connect(
-    mapStateToProps,
-    { getUserData }
-  )(user);
+  componentDidMount() {
+    const handle = this.props.match.params.handle;
+    const screamId = this.props.match.params.screamId;
+
+    if (screamId) this.setState({ screamIdParam: screamId });
+
+    this.props.getUserData(handle);
+    axios
+      .get(`/user/${handle}`)
+      .then((res) => {
+        this.setState({
+          profile: res.data.user
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    const { screams, loading } = this.props.data;
+    const { screamIdParam } = this.state;
+
+    const screamsMarkup = loading ? (
+      <ScreamSkeleton />
+    ) : screams === null ? (
+      <p>No screams from this user</p>
+    ) : !screamIdParam ? (
+      screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
+    ) : (
+      screams.map((scream) => {
+        if (scream.screamId !== screamIdParam)
+          return <Scream key={scream.screamId} scream={scream} />;
+        else return <Scream key={scream.screamId} scream={scream} openDialog />;
+      })
+    );
+
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {screamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          {this.state.profile === null ? (
+            <ProfileSkeleton />
+          ) : (
+            <StaticProfile profile={this.state.profile} />
+          )}
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+user.propTypes = {
+  getUserData: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  data: state.data
+});
+
+export default connect(
+  mapStateToProps,
+  { getUserData }
+)(user);
