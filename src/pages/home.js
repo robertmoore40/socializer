b7c63d6efa3c759e59c5f5a083de54d5e510e4dd
robertmoore40 <robertcmoore92@gmail.com>
2020-09-22 08:44:38 -0400

    imported components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,5 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
+import Scream from '../components/scream/Scream';
+import Profile from '../components/profile/Profile';
