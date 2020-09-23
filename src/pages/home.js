2353fec545140447a87b784b7404ce675e147139
robertmoore40 <robertcmoore92@gmail.com>
2020-09-23 19:23:55 -0400

    formatted document

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
+
 import Scream from '../components/scream/Scream';
 import Profile from '../components/profile/Profile';
 import ScreamSkeleton from '../util/ScreamSkeleton';
@@ -9,28 +10,27 @@ import { connect } from 'react-redux';
 import { getScreams } from '../redux/actions/dataActions';
 
 class home extends Component {
-    componentDidMount() {
-      this.props.getScreams();
-    }
-    render() {
-        const { screams, loading } = this.props.data;
-        let recentScreamsMarkup = !loading ? (
-          screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
-        ) : (
-          <ScreamSkeleton />
-        );
-        return (
-          <Grid container spacing={16}>
-            <Grid item sm={8} xs={12}>
-              {recentScreamsMarkup}
-            </Grid>
-            <Grid item sm={4} xs={12}>
-              <Profile />
-            </Grid>
-          </Grid>
-        );
-      }
-    }
+  componentDidMount() {
+    this.props.getScreams();
+  }
+  render() {
+    const { screams, loading } = this.props.data;
+    let recentScreamsMarkup = !loading ? (
+      screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
+    ) : (
+      <ScreamSkeleton />
+    );
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <Profile />
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 home.propTypes = {
