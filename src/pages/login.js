a6c7563c1a60d8390e4945035d0c065cd364ccf9
robertmoore40 <robertcmoore92@gmail.com>
2020-09-23 19:26:21 -0400

    added redux

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,3 +9,6 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions';
