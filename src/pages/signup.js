b4688a25fb1f42701824484c7db5491d25a65d2f
robertmoore40 <robertcmoore92@gmail.com>
2020-10-13 11:25:15 -0400

    imported

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,2 +1,4 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import AppIcon from '../images/icon.png';
