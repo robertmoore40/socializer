aaab1d951ae78d0ae41810d2858729e4f243163d
robertmoore40 <robertcmoore92@gmail.com>
2020-10-13 11:25:55 -0400

    import mui

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,3 +3,6 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import AppIcon from '../images/icon.png';
 import { Link } from 'react-router-dom';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
