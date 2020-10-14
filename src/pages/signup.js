94cc8a81ff9287011ee7ff014c9e696e64261710
robertmoore40 <robertcmoore92@gmail.com>
2020-10-14 08:37:38 -0400

    add styling

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -12,3 +12,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { connect } from 'react-redux';
 import { signupUser } from '../redux/actions/userActions';
+
+const styles = (theme) => ({
+  ...theme
+});
